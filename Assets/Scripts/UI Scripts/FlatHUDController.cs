12b103c03429c7dc5267badd61d918af16f958d3
Bryan Tai <redhatpieman@gmail.com>
2018-08-02 11:36:30 -0700

    Created FlatHUDController to handle ingredient models

---
+++ Assets/Scripts/UI Scripts/FlatHUDController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//FlatHUDController handles the visibility of the IngredientModels, the Timer, and the Score
+//Timer is handled by its own script
+public class FlatHUDController : MonoBehaviour {
+
+    public GameController gameController;
+
+    //TODO replace with array
+    public GameObject BunModel;
+    public GameObject PattyModel;
+    public GameObject LettuceModel;
+    public GameObject KetchupModel;
+
+    private Dictionary<IngredientType, GameObject> ingredientModels;
+
+    // Use this for initialization
+    void Awake ()
+    {
+        //TODO Replace with LevelParams
+        ingredientModels = new Dictionary<IngredientType, GameObject>();
+        ingredientModels.Add(IngredientType.bun, BunModel);
+        ingredientModels.Add(IngredientType.patty, PattyModel);
+        ingredientModels.Add(IngredientType.lettuce, LettuceModel);
+        ingredientModels.Add(IngredientType.ketchup, KetchupModel);
+    }
+
+    void Start()
+    {
+        hideAllModels();
+    }
+
+    private void hideAllModels()
+    {
+        foreach(GameObject model in ingredientModels.Values)
+        {
+            hideModel(model);
+        }
+    }
+
+    private void hideModel(GameObject model)
+    {
+        model.SetActive(false);
+    }
+
+    //TODO Shrink model til it disappears
+    private void shrinkModel(GameObject model)
+    {
+        model.SetActive(false);
+    }
+
+    private void showModel(GameObject model)
+    {
+        model.SetActive(true);
+    }
+
+
+    public void ActivateIngredientModel(IngredientType ingredientType)
+    {
+        showModel(ingredientModels[ingredientType]);
+    }
+}
