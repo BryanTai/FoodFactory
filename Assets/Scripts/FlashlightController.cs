f12de14d810e6a414317e10085c99427abc86645
Bryan Tai <redhatpieman@gmail.com>
2018-08-18 16:27:28 -0700

    Added Flashlight toggle for dark environments

---
+++ Assets/Scripts/FlashlightController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using Vuforia;
+
+public class FlashlightController : MonoBehaviour {
+
+    private bool torchIsOn = false;
+
+    public void ToggleFlashlight()
+    {
+        torchIsOn = !torchIsOn;
+        CameraDevice.Instance.SetFlashTorchMode(torchIsOn);
+    }
+	
+}
